d31cfbae18ab240c72511026c299518d8abf7c1b
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-02-17 10:05:56 +1100

    slide 3 done

--- scripts/datasaurus-animation.R
+++ scripts/datasaurus-animation.R
@@ -15,13 +15,13 @@ g2 <- datasaurus_dozen |>
          meany = mean(y),
          sdx = sd(x),
          sdy = sd(y),
-         text = glue::glue("correlation: {cor}\nmean x: {meanx}\nmean y: {meany}\nsd x: {sdx}\nsd y: {sdy}"),
+         text = glue::glue("r: {cor}\nmean x: {meanx}\nmean y: {meany}\nsd x: {sdx}\nsd y: {sdy}"),
          .by = dataset) |> 
   ggplot(aes(x=x, y=y))+
   geom_point(color = "white") + 
   geom_text(aes(label = text), x = 20, y = 90, hjust = 0, vjust = 1,
             data = ~. |> slice(1, .by = dataset), 
-            color = "red2", size = 8) +
+            color = "black", size = 8) +
   theme_void(base_size = 24) +
   transition_states(dataset, 3, 1) 
 g2
